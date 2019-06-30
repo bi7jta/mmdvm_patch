32ff750ce916f4a73c7131c8499cdcca55489596
winters_huang <winters_huang>
2019-07-01 02:57:49 +0800

    init

---
+++ add_logs_download_and_disable_passowrd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+#Purpose 
+#http://pi-star:2222
+
+#user: pi-star
+#password: raspberry
+
+rpi-rw;
+cd ~
+curl -OL https://github.com/bi7jta/mmdvm_patch/raw/master/pi-star;
+rpi-rw;
+sudo cp -f ~/pi-star /etc/nginx/sites-enabled/pi-star;
+sudo ln -s /etc /var/log/pi-star/conf  
+sudo nginx -s reload
