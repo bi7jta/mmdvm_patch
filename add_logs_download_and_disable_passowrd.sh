6f9aa0f8d06719c5fc58fe332cc5034f5ae19a4e
winters_huang <winters_huang>
2019-07-02 13:30:13 +0800

    // Make the root filesystem writable 	system('sudo mount -o remount,rw /');

--- add_logs_download_and_disable_passowrd.sh
+++ add_logs_download_and_disable_passowrd.sh
@@ -5,9 +5,12 @@
 #user: pi-star
 #password: raspberry
 
+#Make the root filesystem writable
+
+sudo mount -o remount,rw / ;
+	
 cd ~
 curl -OL https://github.com/bi7jta/mmdvm_patch/raw/master/pi-star;
-rpi-rw;
 sudo cp -f ~/pi-star /etc/nginx/sites-enabled/pi-star;
 sudo ln -s /etc /var/log/pi-star/conf  
 sudo nginx -s reload
